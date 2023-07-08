e01a7bb51d6ab77fc60aaeaa0172d6617c1ed341
Ryan Zidago <46972947+ryanzidago@users.noreply.github.com>
2023-07-08 20:12:40 +0200

    Added state for contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,19 +1,31 @@
 "use client";
 
 import TextInput from "@/components/text_input";
-import Button from "@/components/button";
-import Link from "next/link";
+import { useState } from "react";
 
 export default function Contact() {
+  const [contact, setContact] = useState({});
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const key = event.target.name;
+    const value = event.target.value;
+    setContact({ ...contact, [key]: value });
+  }
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    console.log(contact);
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit} className="w-full max-w-xl">
       <div className="flex flex-col justify-center items-center gap-8 w-full max-w-xl text-slate-700">
         <h1 className="text-xl">Personal Information</h1>
-        <TextInput label="First name" />
-        <TextInput label="Last name" />
-        <TextInput label="Email" />
+        <TextInput id="first_name" label="First name" onChange={handleChange} />
+        <TextInput id="last_name" label="Last name" onChange={handleChange} />
+        <TextInput id="email" label="Email" onChange={handleChange} />
         <div className="flex w-full justify-end">
-          <Button href="work_experience">Next {">"}</Button>
+          <button type="submit">Submit</button>
         </div>
       </div>
     </form>
--- src/components/text_input.tsx
+++ src/components/text_input.tsx
@@ -2,6 +2,8 @@ interface TextInputProps {
   label: string;
   placeholder?: string;
   value?: string;
+  id: string;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 export default function TextInput(props: TextInputProps) {
@@ -10,8 +12,11 @@ export default function TextInput(props: TextInputProps) {
       <label className="flex flex-col gap-2 w-full">
         <span>{props.label}</span>
         <input
+          id={props.id}
+          name={props.id}
           type="text"
           className="border border-slate-200 drop-shadow-sm rounded-md p-2 focus:outline focus:outline-sky-200"
+          onChange={props.onChange}
         />
       </label>
     </div>
