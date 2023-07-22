b8a41615a0d71b35eff9506cbde28984fb30695b
Ryan Zidago <46972947+ryanzidago@users.noreply.github.com>
2023-07-22 13:24:31 +0200

    Improved json resume

--- src/app/resumes/json/page.tsx
+++ src/app/resumes/json/page.tsx
@@ -1,29 +1,12 @@
-const DUMMY_DATA = {
-  firstName: "Jean",
-  lastName: "Jacques",
-  email: "jean.jacques@email.fr",
-  workEntries: [
-    {
-      title: "Senior Software Engineer",
-      companyName: "Some Corp",
-      startDate: "august 2015",
-      endDate: "august ",
-      description: "- Did lots of work there\n- It was fun",
-      location: "Remote",
-    },
-  ],
-  educationEntries: [
-    {
-      degree: "Bootcamp",
-      institutionName: "The Hacking Project",
-      startDate: "july 2015",
-      endDate: "may 2019",
-      description: "- Released some cool code",
-      location: "Paris",
-    },
-  ],
-};
-
+const DUMMY_DATA = require("@/data/resume.json");
 export default function Page() {
-  return <pre>{JSON.stringify(DUMMY_DATA, null, 2)}</pre>;
+  return (
+    <div className="grid grid-cols-3 text-sm">
+      <div />
+      <pre className="p-10 overflow-auto shadow-xl rounded">
+        {JSON.stringify(DUMMY_DATA, null, 2)}
+      </pre>
+      <div />
+    </div>
+  );
 }
