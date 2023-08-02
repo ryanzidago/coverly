cb016561984027025fab5c763e522b79d8115a46
Ryan Zidago <46972947+ryanzidago@users.noreply.github.com>
2023-08-02 07:36:39 +0200

    Fixed empty string warning

--- src/app/resumes/template1/link.tsx
+++ src/app/resumes/template1/link.tsx
@@ -2,7 +2,7 @@ export default function Link(props) {
   return (
     <a
       className="text-sky-500 font-light hover:text-sky-300 duration-200"
-      href={props.href}
+      href={props.href.length === 0 ? "#" : props.href}
       target={props.target || "_blank"}
     >
       {props.children}
