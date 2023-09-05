773a8935bf1c1caf45299c031dabe4a9f931551e
Ryan Zidago <46972947+ryanzidago@users.noreply.github.com>
2023-09-05 09:16:54 +0200

    Stages are now sortable

--- src/app/applications-pipeline/container.tsx
+++ src/app/applications-pipeline/container.tsx
@@ -5,8 +5,15 @@ import {
 } from "@dnd-kit/sortable";
 
 import { Sortable } from "@/components/dnd/sortable";
+import { SortableKind } from "./page";
 
-export default function Container({ id, applications, activeId, stage }) {
+export default function Container({
+  id,
+  activeStageId,
+  applications,
+  activeApplicationId,
+  stage,
+}) {
   const { setNodeRef } = useDroppable({ id });
 
   return (
@@ -15,22 +22,30 @@ export default function Container({ id, applications, activeId, stage }) {
       items={applications}
       strategy={verticalListSortingStrategy}
     >
-      <div className="flex flex-col text-center gap-8">
+      <div
+        className={
+          "flex flex-col text-center gap-8 0 h-96  " +
+          (stage.id === activeStageId ? "opacity-50" : "")
+        }
+      >
         <div>{stage.title}</div>
         <div
           ref={setNodeRef}
-          className="bg-slate-50 p-2 h-full text-center flex flex-col gap-8"
+          className="bg-slate-50 h-96 p-2 text-center flex flex-col gap-8"
         >
           {applications.map((application) => (
             <Sortable
               key={application.id}
               id={application.id}
-              data={{ stageId: application.stageId }}
+              data={{
+                stageId: application.stageId,
+                sortableKind: SortableKind.Application,
+              }}
             >
               <div
                 className={
                   "bg-sky-400 rounded shadow-md px-2 py-4 " +
-                  (application.id === activeId && "opacity-60")
+                  (application.id === activeApplicationId && "opacity-60")
                 }
               >
                 {application.organisation}
--- src/app/applications-pipeline/page.tsx
+++ src/app/applications-pipeline/page.tsx
@@ -17,21 +17,28 @@ import {
   useSensors,
 } from "@dnd-kit/core";
 
-import { arrayMove, sortableKeyboardCoordinates } from "@dnd-kit/sortable";
+import {
+  SortableContext,
+  arrayMove,
+  horizontalListSortingStrategy,
+  sortableKeyboardCoordinates,
+} from "@dnd-kit/sortable";
+
+import { Sortable } from "@/components/dnd/sortable";
 
 import Container from "./container";
 
 const STAGES = [
-  { id: 1, title: "Backlog" },
-  { id: 2, title: "First contact" },
-  { id: 3, title: "Phone screen" },
-  { id: 4, title: "1st interview round" },
-  { id: 5, title: "2nd interview round" },
-  { id: 6, title: "3rd interview round" },
-  { id: 7, title: "Application declined" },
-  { id: 8, title: "Offer received" },
-  { id: 9, title: "Offer declined" },
-  { id: 10, title: "Offer accepted" },
+  { id: 1, position: 2, title: "Backlog" },
+  { id: 2, position: 1, title: "First contact" },
+  { id: 3, position: 3, title: "Phone screen" },
+  { id: 4, position: 4, title: "1st interview round" },
+  { id: 5, position: 5, title: "2nd interview round" },
+  { id: 6, position: 6, title: "3rd interview round" },
+  { id: 7, position: 7, title: "Application declined" },
+  { id: 8, position: 8, title: "Offer received" },
+  { id: 9, position: 9, title: "Offer declined" },
+  { id: 10, position: 10, title: "Offer accepted" },
 ];
 
 const APPLICATIONS = [
@@ -48,10 +55,16 @@ const APPLICATIONS = [
   { id: 111, organisation: "Flexport", stageId: 1 },
 ];
 
+export enum SortableKind {
+  Stage,
+  Application,
+}
+
 export default function Page() {
   const [stages, setStages] = useState(STAGES);
   const [applications, setApplications] = useState(APPLICATIONS);
-  const [activeId, setActiveId] = useState<null | number>(null);
+  const [activeApplicationId, setActiveApplicationId] = useState(null);
+  const [activeStageId, setActiveStageId] = useState(null);
 
   const sensors = useSensors(
     useSensor(PointerSensor),
@@ -60,14 +73,49 @@ export default function Page() {
     }),
   );
 
+  useEffect(() => {
+    setStages((prevStages) => {
+      return prevStages.slice().sort((a, b) => {
+        if (a.position < b.position) return -1;
+        if (a.position > b.position) return 1;
+        return 0;
+      });
+    });
+  }, []);
+
   function activeApplicationCard(id) {
     const application = findApplication(id);
-
-    console.log("activeApplication", application);
 
     return (
       <div className="bg-sky-400 rounded shadow-2xl px-2 py-4 flex flex-col text-center">
         {application?.organisation}
+      </div>
+    );
+  }
+
+  function activeStageCard(id) {
+    const stage = findStage(id);
+    return (
+      <div className="flex flex-col text-center gap-8 0 h-96">
+        <div>{stage.title}</div>
+        <div className="bg-slate-50 h-96 p-2 text-center flex flex-col gap-8">
+          {applications
+            .filter((application) => application.stageId === stage.id)
+            .map((application) => (
+              <Sortable
+                key={application.id}
+                id={application.id}
+                data={{
+                  stageId: application.stageId,
+                  sortableKind: SortableKind.Application,
+                }}
+              >
+                <div className={"bg-sky-400 rounded shadow-md px-2 py-4"}>
+                  {application.organisation}
+                </div>
+              </Sortable>
+            ))}
+        </div>
       </div>
     );
   }
@@ -81,25 +129,36 @@ export default function Page() {
       onDragEnd={handleDragEnd}
       onDragCancel={handleDragCancel}
     >
-      <div
-        className={
-          "text-xs w-full overflow-scroll " +
-          `grid grid-cols-${stages.length} gap-8`
-        }
-      >
-        {stages.map((stage) => (
-          <Container
-            key={stage.id}
-            id={stage.id}
-            stage={stage}
-            activeId={activeId}
-            applications={applications.filter(
-              (application) => application.stageId === stage.id,
-            )}
-          />
-        ))}
-        <DragOverlay>{activeId && activeApplicationCard(activeId)}</DragOverlay>
-      </div>
+      <SortableContext items={stages} strategy={horizontalListSortingStrategy}>
+        <div
+          className={
+            "text-xs w-full overflow-scroll " +
+            `grid grid-cols-${stages.length} gap-8`
+          }
+        >
+          {stages.map((stage) => (
+            <Sortable
+              key={stage.id}
+              id={stage.id}
+              data={{ stageId: stage.id, sortableKind: SortableKind.Stage }}
+            >
+              <Container
+                id={stage.id}
+                stage={stage}
+                activeStageId={activeStageId}
+                activeApplicationId={activeApplicationId}
+                applications={applications.filter(
+                  (application) => application.stageId === stage.id,
+                )}
+              />
+            </Sortable>
+          ))}
+          <DragOverlay>
+            {activeApplicationId && activeApplicationCard(activeApplicationId)}
+            {activeStageId && activeStageCard(activeStageId)}
+          </DragOverlay>
+        </div>
+      </SortableContext>
     </DndContext>
   );
 
@@ -112,12 +171,69 @@ export default function Page() {
   }
 
   function handleDragStart(event: DragStartEvent) {
-    const activeId = event.active.id;
-    console.log(event.active.id);
-    setActiveId(activeId);
+    const sortableKind = event?.active?.data?.current?.sortableKind;
+
+    if (sortableKind === SortableKind.Stage) {
+      handleStageDragStart(event);
+    }
+
+    if (sortableKind === SortableKind.Application) {
+      handleApplicationDragStart(event);
+    }
+  }
+
+  function handleStageDragStart(event) {
+    const activeStageId = event.active.id;
+    setActiveStageId(activeStageId);
+  }
+
+  function handleApplicationDragStart(event) {
+    const activeApplicationId = event.active.id;
+    setActiveApplicationId(activeApplicationId);
   }
 
   function handleDragOver(event: DragOverEvent) {
+    const sortableKind = event?.active?.data?.current?.sortableKind;
+
+    if (sortableKind === SortableKind.Application) {
+      handleApplicationDragOver(event);
+    }
+
+    if (sortableKind === SortableKind.Stage) {
+      handleStageDragOver(event);
+    }
+  }
+
+  function handleStageDragOver(event) {
+    const { active, over } = event;
+    const activeStage = findStage(active.id);
+    const overStage = findStage(over.id);
+    if (!activeStage || !overStage || activeStage.id == overStage.id) {
+      return;
+    }
+    const activeStagePosition = activeStage.position;
+    const overStagePosition = overStage.position;
+    setStages((prevStages) => {
+      return prevStages
+        .slice()
+        .map((stage) => {
+          if (stage.id === activeStage.id) {
+            return { ...activeStage, position: overStagePosition };
+          }
+          if (stage.id === overStage.id) {
+            return { ...overStage, position: activeStagePosition };
+          }
+          return stage;
+        })
+        .sort((a, b) => {
+          if (a.position < b.position) return -1;
+          if (a.position > b.position) return 1;
+          return 0;
+        });
+    });
+  }
+
+  function handleApplicationDragOver(event) {
     // here, active and over are always the applications being moved
     const { active, over } = event;
 
@@ -140,13 +256,9 @@ export default function Page() {
       return;
     }
 
-    setStages((prevStages) => {
-      return prevStages.concat([]);
-    });
-
     setApplications((prevApplications) => {
       const updatedApplications = prevApplications.map((application) => {
-        if (application.id === activeApplication.id) {
+        if (application.id === activeApplication?.id) {
           return {
             ...application,
             stageId: overStage.id,
@@ -160,10 +272,52 @@ export default function Page() {
   }
 
   function handleDragEnd(event: DragEndEvent) {
-    setActiveId(null);
+    const sortableKind = event?.active?.data?.current?.sortableKind;
+
+    if (sortableKind === SortableKind.Application) {
+      handleApplicationDragEnd(event);
+    }
+
+    if (sortableKind === SortableKind.Stage) {
+      handleStageDragEnd(event);
+    }
+  }
+
+  function handleApplicationDragEnd(event) {
+    setActiveApplicationId(null);
+  }
+
+  function handleStageDragEnd(event) {
+    setActiveStageId(null);
+    // const { active, over } = event;
+    // const activeStage = findStage(active.id);
+    // const overStage = findStage(over.id);
+    // if (!activeStage || !overStage || activeStage.id == overStage.id) {
+    //   return;
+    // }
+    // const activeStagePosition = activeStage.position;
+    // const overStagePosition = overStage.position;
+    // setStages((prevStages) => {
+    //   return prevStages
+    //     .map((stage) => {
+    //       if (stage.id === activeStage.id) {
+    //         return { ...activeStage, position: overStagePosition };
+    //       }
+    //       if (stage.id === overStage.id) {
+    //         return { ...overStage, position: activeStagePosition };
+    //       }
+    //       return stage;
+    //     })
+    //     .sort((a, b) => {
+    //       if (a.position < b.position) return -1;
+    //       if (a.position > a.position) return 1;
+    //       return 0;
+    //     });
+    // });
   }
 
   function handleDragCancel(event: DragCancelEvent) {
-    setActiveId(null);
+    setActiveApplicationId(null);
+    setActiveStageId(null);
   }
 }
