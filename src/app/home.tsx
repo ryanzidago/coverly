6a3d22f1289e0c8c5b8ca832df45989fb475c22b
Ryan Zidago <46972947+ryanzidago@users.noreply.github.com>
2023-10-30 18:38:23 +0100

    Cleaned up UI / code

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -2,16 +2,14 @@
 import { Popover } from "@headlessui/react";
 import Image from "next/image";
 import Link from "next/link";
-import { useSession, signIn, signOut } from "next-auth/react";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
-  const { data: session, status } = useSession();
-  // console.log(session, status);
-
+  const { status } = useSession();
   const isAuthenticated = status === "authenticated";
 
   return (
-    <Popover className={"relative"}>
+    <Popover className={"relative p-2"}>
       <Popover.Button>
         <Image src={`/home.svg`} width={20} height={20} alt="home button" />
       </Popover.Button>
