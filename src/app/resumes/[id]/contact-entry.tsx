6dd9e79c2dab65bca9edf4ef3f43c9d0f7af851f
Ryan Zidago <46972947+ryanzidago@users.noreply.github.com>
2023-11-02 22:03:06 +0100

    Styled contact entry

--- src/app/resumes/[id]/contact-entry.tsx
+++ src/app/resumes/[id]/contact-entry.tsx
@@ -18,47 +18,56 @@ export default function ContactEntry({ resume }) {
       id="contact-entry-form"
       action={updateContactEntry}
       onChange={handleOnChange}
+      className="flex flex-col gap-2"
     >
       <input
         type="hidden"
         name="contactEntryId"
         value={resume.contactEntry.id}
       />
-      <input
-        type="text"
-        name="firstName"
-        placeholder="First Name"
-        defaultValue={resume?.contactEntry?.firstName}
-      />
-      <input
-        type="text"
-        name="lastName"
-        placeholder="Last Name"
-        defaultValue={resume?.contactEntry?.lastName}
-      />
+      <div className="flex flex-row gap-2">
+        <input
+          type="text"
+          className="p-1 rounded hover:shadow-inner "
+          name="firstName"
+          placeholder="First Name"
+          defaultValue={resume?.contactEntry?.firstName}
+        />
+        <input
+          type="text"
+          className="p-1 rounded hover:shadow-inner "
+          name="lastName"
+          placeholder="Last Name"
+          defaultValue={resume?.contactEntry?.lastName}
+        />
+      </div>
       <input
         type="text"
         name="email"
         placeholder="Email Address"
+        className="p-1 rounded hover:shadow-inner w-56 "
         defaultValue={resume?.contactEntry?.email}
       />
-      <div>
+      <div className="flex gap-2">
         <input
           type="tel"
           name="countryCode"
           placeholder="Country Code"
+          className="p-1 rounded hover:shadow-inner "
           defaultValue={resume?.contactEntry?.phoneNumber?.countryCode}
         />
         <input
           type="tel"
           name="number"
           placeholder="Phone Number"
+          className="p-1 rounded hover:shadow-inner "
           defaultValue={resume?.contactEntry?.phoneNumber?.number}
         />
       </div>
-      <div>
+      <div className="flex gap-2">
         <input
           type="text"
+          className="hover:shadow-inner p-1 rounded "
           name="city"
           placeholder="City"
           defaultValue={resume?.contactEntry?.location?.city}
@@ -67,6 +76,7 @@ export default function ContactEntry({ resume }) {
           type="text"
           name="country"
           placeholder="Country"
+          className="hover:shadow-inner p-1 rounded "
           defaultValue={resume?.contactEntry?.location?.country}
         />
       </div>
