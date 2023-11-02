3a829050390182e7cb2b5b64db662c2edeae7bee
Ryan Zidago <46972947+ryanzidago@users.noreply.github.com>
2023-11-02 17:17:35 +0100

    Filter out hidden achievements

--- src/app/resumes/template1/entry-description.tsx
+++ src/app/resumes/template1/entry-description.tsx
@@ -6,11 +6,13 @@
 export default function EntryAchievements(props) {
   return (
     <ul className="p-4">
-      {props.achievements.map((achievement, index: number) => (
-        <li key={index + props.index} className="text-justify">
-          {achievement.description}
-        </li>
-      ))}
+      {props.achievements
+        .filter((achievement) => achievement.displayed)
+        .map((achievement, index: number) => (
+          <li key={index + props.index} className="text-justify">
+            {achievement.description}
+          </li>
+        ))}
     </ul>
   );
 }
