1a605552713c5218a9715dafc8726546ece58ff1
Ryan Zidago <46972947+ryanzidago@users.noreply.github.com>
2023-11-02 17:16:51 +0100

    Added displayed to achievements

---
+++ prisma/migrations/20231031185459_add_displayed_to_achievements/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "education_achievements" ADD COLUMN     "displayed" BOOLEAN NOT NULL DEFAULT true;
+
+-- AlterTable
+ALTER TABLE "work_achievements" ADD COLUMN     "displayed" BOOLEAN NOT NULL DEFAULT true;
