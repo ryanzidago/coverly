5b7778dc6d501e1bcc6f7e95430f45f729062db7
Ryan Zidago <46972947+ryanzidago@users.noreply.github.com>
2023-11-03 15:14:19 +0100

    Deleted unused code

--- src/app/resumes/template1/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import Template1 from "./template1";
-
-const FORM_DATA = require("@/data/db.json");
-
-export default function Page() {
-  return <Template1 formData={FORM_DATA.resumes[0]} />;
-}
